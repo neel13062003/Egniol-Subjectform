a01cc9fe46f4f608ff1c8aa776676a912218c1e5
NeelSoni <neelnsoni13@gmail.com>
2024-01-31 13:49:12 +0530

    Remove formold

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import FormComponent from "./components/FormComponent";
 import FormComponent1 from "./components/FormComponent1";
 
 function App() {
